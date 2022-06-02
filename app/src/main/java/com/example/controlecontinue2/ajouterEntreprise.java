6971db879360befe3602b4ba4c9c4e87f67585ce
anas-hje <83504382+anas-hje@users.noreply.github.com>
2022-06-02 15:33:06 +0100

    exercice 10 : a-b

--- app/src/main/java/com/example/controlecontinue2/ajouterEntreprise.java
+++ app/src/main/java/com/example/controlecontinue2/ajouterEntreprise.java
@@ -3,12 +3,20 @@ package com.example.controlecontinue2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ajouterEntreprise extends AppCompatActivity {
-
+   EditText RS,ADR,CP;
+   Button Enregistrer,annuller;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_entreprise);
+        RS=findViewById(R.id.txtraison);
+        ADR=findViewById(R.id.txtadresse);
+        CP=findViewById(R.id.txtcapital);
+        Enregistrer=findViewById(R.id.enregistrer);
+        annuller=findViewById(R.id.annuler);
     }
 }
